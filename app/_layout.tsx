23ffd534f50d1c80c599c0993394a44bb7a12ad4
Ali2210 <Alideveloper95@gmail.com>
2025-01-13 10:15:44 +0500

    inital version

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{headerTitle: 'Home', headerTitleAlign: 'center'}} />
+      <Stack.Screen name="not-found" options={{headerTitle: 'Route 404', headerTitleAlign: 'center'}} />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text style={style.text}> WisdomEnigma Connect </Text>
+      <Text style={style.text}> Be alchemist </Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container:{
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  text:{
+    color: '#fff',
+    fontSize: 20
+  },
+  button:{
+    fontSize: 20,
+    color: 'gold',
+    position: 'relative',
+    top: 180,
+  }
+})
